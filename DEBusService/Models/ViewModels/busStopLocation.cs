84c4dfcf1de9c3d7fcfadc86794bf582d983f1ac
dougEpp <doug.epp@gmail.com>
2015-10-14 08:48:58 -0400

    Added busStopLocation viewModel for experimentation

---
+++ DEBusService/Models/ViewModels/busStopLocation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DEBusService.Models.ViewModels
+{
+    public class busStopLocation
+    {
+        private string locationName;
+
+        public string LocationName
+        {
+            get { return locationName; }
+            set { locationName = value; }
+        }
+        private bool hasRoutes;
+
+        public bool HasRoutes
+        {
+            get { return hasRoutes; }
+            set { hasRoutes = value; }
+        }
+
+        public busStopLocation(string locationName, bool hasRoutes)
+        {
+            this.locationName = locationName;
+            this.hasRoutes = hasRoutes;
+        }
+
+    }
+}
